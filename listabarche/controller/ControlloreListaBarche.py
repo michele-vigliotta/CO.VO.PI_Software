3a1ace16d739b17770ca35357760baff62affa1f
erikapignotti <84499902+erikapignotti@users.noreply.github.com>
2021-07-24 12:41:13 +0200

    Update ControlloreListaBarche.py
    modifica controllore lista barche

--- listabarche/controller/ControlloreListaBarche.py
+++ listabarche/controller/ControlloreListaBarche.py
@@ -1,1 +1,22 @@
-#ControlloreListaBarche
+from listabarche.model.ListaBarche import ListaBarche
+
+
+class ControlloreListaBarche():
+    def __init__(self):
+        super(ControlloreListaBarche, self).__init__()
+        self.model = ListaBarche()
+
+    def aggiungi_barca(self, barca):
+        self.model.aggiungi_barca(barca)
+
+    def get_lista_barche(self):
+        return self.model.get_lista_barche()
+
+    def get_barca_by_index(self, index):
+        return self.model.get_barca_by_index(index)
+
+    def elimina_barca_by_targa(self, targa):
+        self.model.rimuovi_barca_by_targa(targa)
+
+    def save_data(self):
+        self.model.save_data()
