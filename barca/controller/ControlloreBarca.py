8f59b3c505a9d75410088b4203ffa5ffc8a43c66
erikapignotti <84499902+erikapignotti@users.noreply.github.com>
2021-07-24 12:15:08 +0200

    Update ControlloreBarca.py
    controllore barca

--- barca/controller/ControlloreBarca.py
+++ barca/controller/ControlloreBarca.py
@@ -1,1 +1,27 @@
-#Controllore Barca
+class ControlloreBarca():
+    def __init__(self, barca):
+        self.model = barca
+
+    def get_nome_barca(self):
+        return self.model.nome
+
+    def get_targa_barca(self):
+        return self.model.targa
+
+    def get_nomereferente_barca(self):
+        return self.model.nomereferente
+
+    def get_cognomereferente_barca(self):
+        return self.model.cognomereferente
+
+    def get_cf_barca(self):
+        return self.model.cf
+
+    def get_telefono_barca(self):
+        return self.model.telefono
+
+    def get_equipaggio_barca(self):
+        return self.model.equipaggio
+
+    def get_licenza_barca(self):
+        return self.model.licenza
